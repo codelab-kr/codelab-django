bb786d3772bd9154e37c073db67a3aa4fb6fce75
codelab-kr <master@code-lab.kr>
2024-07-17 19:28:24 +0900

    [AUTH] apply allauth custom templates

--- common/auth/urls.py
+++ common/auth/urls.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path  # , re_path
+from django.urls import include, path
 
 from .views import UserListView
-
-# from .views import CustomConfirmEmailView
-# from allauth.account import views as account_views
 
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # django-allauth URL 패턴 포함
-    # path('accounts/confirm-email/', CustomConfirmEmailView.as_view(), name='account_confirm_email'),
-    # 비밀번호 재설정
-    # re_path(r'^accounts/password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$', CustomPasswordResetFromKeyView.as_view(), name='account_reset_password_from_key'),
-    # path('accounts/password/reset/key/done/', account_views.password_reset_from_key_done, name='account_reset_password_from_key_done'),
 ]
--- common/auth/views.py
+++ common/auth/views.py
@@ -3,23 +3,7 @@ from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserSerializer
 
-# from allauth.account.views import ConfirmEmailView
-# from allauth.account.views import PasswordResetFromKeyView
-
 
 class UserListView(generics.ListAPIView):
     queryset = CustomUser.objects.all()
-    # `from allauth.account.views import ConfirmEmailView` is importing the `ConfirmEmailView` class from
-    # the `allauth` package, specifically from the `account.views` module. This class is typically used in
-    # Django projects that utilize the django-allauth package for user authentication and account
-    # management. The `ConfirmEmailView` is responsible for handling the email confirmation process for
-    # user accounts, where users need to verify their email addresses before gaining full access to the
-    # application.
     serializer_class = UserSerializer
-
-
-# class CustomConfirmEmailView(ConfirmEmailView):
-#     template_name = 'account/email_confirm.html'
-
-# class CustomPasswordResetFromKeyView(PasswordResetFromKeyView):
-#     template_name = 'account/password_reset_from_key.html'
