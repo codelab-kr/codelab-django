8a7b6c7eba5fa296e689339c052cf6f674bb3eb8
codelab-kr <master@code-lab.kr>
2024-08-01 12:46:43 +0900

    [FEAT] add chat

--- .docs/README-dev-04.md
+++ .docs/README-dev-04.md
@@ -1,4 +1,17 @@
 # 이러닝 시스템 - chat
 ```shell
 python scripts/add_service_app.py edu chat
+
+>>> import channels.layers
+>>> from asgiref.sync import async_to_sync
+>>> cl = channels.layers.get_channel_layer()
+>>> async_to_sync(cl.send)('test', {'message': 'hello'}0
+  File "<console>", line 1
+    async_to_sync(cl.send)('test', {'message': 'hello'}0
+                          ^
+SyntaxError: '(' was never closed
+>>> async_to_sync(cl.send)('test', {'message': 'hello'})
+>>> async_to_sync(cl.receive)('test')
+{'message': 'hello'}
+
 ```
--- services/edu/apps/chat/consumers.py
+++ services/edu/apps/chat/consumers.py
@@ -1,21 +1,46 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.utils import timezone
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
 
-    def connect(self):
+    async def connect(self):
+        self.user = self.scope['user']
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+        # 그룹 참여
+        await self.channel_layer.group_add(  # type: ignore
+            self.room_group_name,
+            self.channel_name,
+        )
         # 연결 수락
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        # 그룹 탈퇴
+        await self.channel_layer.group_discard(  # type: ignore
+            self.room_group_name,
+            self.channel_name,
+        )
 
     # 웹소켓에서 메시지 수신
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        # 그룹에 메시지 전송
+        await self.channel_layer.group_send(  # type: ignore
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username,
+                'datetime': timezone.now().isoformat(),  # iso 8601
+            },
+        )
 
+    # 그룹에서 메시지 수신
+    async def chat_message(self, event):
         # 웹소켓으로 메시지 전송
-        self.send(text_data=json.dumps({'message': message}))
+        await self.send(text_data=json.dumps(event))
